287e63eaa0e3770c47ed4606803e18f9f552f293
Ali durul <alidrl26@gmail.com>
2021-11-19 18:02:25 +0200

    python file uploaded and ass1 added

---
+++ Python/01 (Password Reminder).py
@@ -0,0 +1,7 @@
+name = "Joseph"
+password = "W@12"
+user_name = input("Please enter your name: ").strip().title()
+if user_name == "Joseph":
+    print(f"Hello, {name}! The password is : {password}")
+else:
+    print(f"Hello, {user_name}! See you later.")
